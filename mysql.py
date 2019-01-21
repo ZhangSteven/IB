774f539c904dce6ce5770c664f3fd1a7531d70ce
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2019-01-21 20:07:42 +0800

    execute many records at the same time, insert or update a record

--- mysql.py
+++ mysql.py
@@ -11,9 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 
-
-
-
 if __name__ == '__main__':
 	import logging.config
 	logging.config.fileConfig('logging.config', disable_existing_loggers=False)
@@ -24,15 +21,50 @@ if __name__ == '__main__':
 								db='blp_trade',
 								cursorclass=pymysql.cursors.DictCursor)
 
+	# Works
+	# try:
+	# 	with connection.cursor() as cursor:
+	# 		# create a new record
+	# 		sql = "INSERT INTO `trade_file` (`file_name`, `last_modified`, `status`) \
+	# 				VALUES (%s, %s, %s)"
+	# 		cursor.execute(sql, ('Trade File 20190117.xlsx'
+	# 							, '2019-01-20 10:05:22'
+	# 							, '0')
+	# 						)
+
+	# 		# save changes
+	# 		connection.commit()
+
+	# finally:
+	# 	connection.close()
+
+
+	# Insert many records, also works
+	# records = [('Trade File 20190115.xlsx', '2019-01-18 9:15:38', '0')
+	# 			, ('Trade File 20190116.xlsx', '2019-01-19 15:01:55', '1')]
+	# try:
+	# 	with connection.cursor() as cursor:
+	# 		# create a new record
+	# 		sql = "INSERT INTO `trade_file` (`file_name`, `last_modified`, `status`) \
+	# 				VALUES (%s, %s, %s)"
+	# 		cursor.executemany(sql, records)
+
+	# 		# save changes
+	# 		connection.commit()
+
+	# finally:
+	# 	connection.close()
+
+
+	# insert or replace, many records
+	records = [('Trade File 20190117.xlsx', '2019-01-23 9:28:56', '0')
+			, ('Trade File 20190113.xlsx', '2019-01-14 23:15:32', '1')]
 	try:
 		with connection.cursor() as cursor:
 			# create a new record
-			sql = "INSERT INTO `trade_file` (`file_name`, `last_modified`, `status`) \
+			sql = "REPLACE INTO `trade_file` (`file_name`, `last_modified`, `status`) \
 					VALUES (%s, %s, %s)"
-			cursor.execute(sql, ('Trade File 20190117.xlsx'
-								, '2019-01-20 10:05:22'
-								, '0')
-							)
+			cursor.executemany(sql, records)
 
 			# save changes
 			connection.commit()
