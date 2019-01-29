8fc7fbd88a0d9e4d42eb66459aad0f45668a19cf
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2019-01-29 18:36:50 +0800

    added test for trade group

--- test/test_guangfa.py
+++ test/test_guangfa.py
@@ -3,7 +3,7 @@
 
 import unittest2
 from os.path import join
-from IB.utility import get_current_path
+from IB.utility import get_current_path, toOpenCloseGroup
 from IB.guangfa import fileToRecords, createTradeRecords
 from datetime import datetime
 
@@ -37,6 +37,7 @@ class TestGF(unittest2.TestCase):
         self.assertAlmostEqual(3.80, records[4]['Price'])
 
 
+
     def testCreateTradeRecords2(self):
         records = list(createTradeRecords(join(get_current_path(), 'samples',
             '8888802200trddata_f20140131.txt')))
@@ -44,31 +45,19 @@ class TestGF(unittest2.TestCase):
         self.verifyTradeRecord2(records[5])
 
 
-    # def testRecordGroups(self):
-    #     """
-    #     After sorting, 6th and 7th records form a box position, therefore all
-    #     records should be separated into 2 groups.
-    #     """
-    #     groups = toRecordGroups(createTradeRecords(join(get_current_path(), 'samples', 'trade2',
-    #         'DU1237908.Trades_TradeConfirmFlex.2.csv')))
-    #     self.assertEqual(len(groups), 2)
-    #     self.assertEqual(len(groups[0]), 6)     # 1st group has 6 trades
-    #     self.assertEqual(len(groups[1]), 22)    # 2nd group has 22 trades
-    #     self.verifyTrade3(groups[0][5])
-    #     self.verifyTrade4(groups[1][0])
 
-
-
-    # def testRecordGroups2(self):
-    #     """
-    #     It will split to 3 groups.
-    #     """
-    #     groups = toRecordGroups(createTradeRecords(join(get_current_path(), 'samples', 'trade3',
-    #         'DU1237908.Trades_TradeConfirmFlex.3.csv')))
-    #     self.assertEqual(len(groups), 3)
-    #     self.assertEqual(len(groups[0]), 6)     # 1st group has 6 trades
-    #     self.assertEqual(len(groups[1]), 6)     # 2nd group has 6 trades
-    #     self.assertEqual(len(groups[2]), 4)
+    def testRecordGroups(self):
+        """
+        After sorting, 6th and 7th records form a box position, therefore all
+        records should be separated into 2 groups.
+        """
+        groups = list(toOpenCloseGroup(createTradeRecords(join(get_current_path(), 
+                        'samples', '8888802200trddata_f20140131.txt'))))
+        self.assertEqual(len(groups), 2)
+        self.assertEqual(len(groups[0]), 1)     # 1st group has 1 trade
+        self.assertEqual(len(groups[1]), 5)     # 2nd group has 5 trades
+        self.verifyTradeRecord2(groups[0][0])
+        self.verifyTradeRecord3(groups[1][4])
 
 
 
@@ -125,7 +114,8 @@ class TestGF(unittest2.TestCase):
 
     def verifyTradeRecord2(self, record):
         """
-        first trade
+        last trade in file (before sorting), 1st trade in 1st trade group (after
+        trading)
         """
         self.assertEqual(len(record), 10)   # there should be 10 fields
         self.assertEqual('S K4 Comdty', record['BloombergTicker'])
@@ -135,3 +125,18 @@ class TestGF(unittest2.TestCase):
         self.assertEqual(datetime(2014,1,31), record['TradeDate'])
         self.assertEqual(datetime(2014,1,31), record['SettlementDate'])
         self.assertEqual(datetime(2014,1,31,hour=22,minute=47,second=36), record['Datetime'])
+
+
+
+    def verifyTradeRecord3(self, record):
+        """
+        last trade in 2nd trade group (after trading)
+        """
+        self.assertEqual(len(record), 10)   # there should be 10 fields
+        self.assertEqual('COH4 Comdty', record['BloombergTicker'])
+        self.assertEqual('Cover', record['Side'])
+        self.assertEqual(1, record['Quantity'])
+        self.assertAlmostEqual(106.95, record['Price'])
+        self.assertEqual(datetime(2014,1,31), record['TradeDate'])
+        self.assertEqual(datetime(2014,1,31), record['SettlementDate'])
+        self.assertEqual(datetime(2014,1,31,hour=21,minute=32,second=2), record['Datetime'])
