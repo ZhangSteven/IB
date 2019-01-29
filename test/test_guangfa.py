a7d94fda1a6e85108ab267b9e4543618107346d7
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2019-01-29 18:22:24 +0800

    sample data and test cases

---
+++ test/test_guangfa.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+# 
+
+import unittest2
+from os.path import join
+from IB.utility import get_current_path
+from IB.guangfa import fileToRecords, createTradeRecords
+from datetime import datetime
+
+
+
+class TestGF(unittest2.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestGF, self).__init__(*args, **kwargs)
+
+
+    def testFileToRecords(self):
+        records = fileToRecords(join(get_current_path(), 'samples',
+            'XXXtrddata_f20130603.txt'))
+        self.assertEqual(len(records), 20)
+        self.verifyFileRecord1(records[0])
+        self.verifyFileRecord2(records[19])
+
+
+
+    def testCreateTradeRecords(self):
+        records = list(createTradeRecords(join(get_current_path(), 'samples',
+            'XXXtrddata_f20130607.txt')))
+        self.assertEqual(len(records), 19)
+        self.verifyTradeRecord1(records[0])
+
+        # make sure sorting works
+        self.assertAlmostEqual(3.83, records[1]['Price'])
+        self.assertAlmostEqual(3.85, records[2]['Price'])
+        self.assertAlmostEqual(3.86, records[3]['Price'])
+        self.assertAlmostEqual(3.80, records[4]['Price'])
+
+
+    def testCreateTradeRecords2(self):
+        records = list(createTradeRecords(join(get_current_path(), 'samples',
+            '8888802200trddata_f20140131.txt')))
+        self.assertEqual(len(records), 6)
+        self.verifyTradeRecord2(records[5])
+
+
+    # def testRecordGroups(self):
+    #     """
+    #     After sorting, 6th and 7th records form a box position, therefore all
+    #     records should be separated into 2 groups.
+    #     """
+    #     groups = toRecordGroups(createTradeRecords(join(get_current_path(), 'samples', 'trade2',
+    #         'DU1237908.Trades_TradeConfirmFlex.2.csv')))
+    #     self.assertEqual(len(groups), 2)
+    #     self.assertEqual(len(groups[0]), 6)     # 1st group has 6 trades
+    #     self.assertEqual(len(groups[1]), 22)    # 2nd group has 22 trades
+    #     self.verifyTrade3(groups[0][5])
+    #     self.verifyTrade4(groups[1][0])
+
+
+
+    # def testRecordGroups2(self):
+    #     """
+    #     It will split to 3 groups.
+    #     """
+    #     groups = toRecordGroups(createTradeRecords(join(get_current_path(), 'samples', 'trade3',
+    #         'DU1237908.Trades_TradeConfirmFlex.3.csv')))
+    #     self.assertEqual(len(groups), 3)
+    #     self.assertEqual(len(groups[0]), 6)     # 1st group has 6 trades
+    #     self.assertEqual(len(groups[1]), 6)     # 2nd group has 6 trades
+    #     self.assertEqual(len(groups[2]), 4)
+
+
+
+    def verifyFileRecord1(self, record):
+        """
+        First trade
+        """
+        self.assertEqual(len(record), 22)   # there should be 22 fields
+        self.assertEqual('2013-06-03', record['SettlementDate'])
+        self.assertEqual('2013-06-03', record['TradeDate'])
+        self.assertEqual('USD', record['Currency'])
+        self.assertEqual('CME', record['Exchange'])
+        self.assertEqual('AD1306', record['Contract'])
+        self.assertEqual('L', record['OpenClose'])
+        self.assertEqual('1', record['BuyQuantity'])
+        self.assertEqual('0.96', record['Price'])
+        self.assertEqual('96060.00', record['Amount'])
+
+
+
+    def verifyFileRecord2(self, record):
+        """
+        Last trade
+        """
+        self.assertEqual(len(record), 22)   # there should be 22 fields
+        self.assertEqual('2013-06-03', record['SettlementDate'])
+        self.assertEqual('2013-06-03', record['TradeDate'])
+        self.assertEqual('USD', record['Currency'])
+        self.assertEqual('CME', record['Exchange'])
+        self.assertEqual('GC1308', record['Contract'])
+        self.assertEqual('O', record['OpenClose'])
+        self.assertEqual('1', record['BuyQuantity'])
+        self.assertEqual('0', record['SellQuantity'])
+        self.assertEqual('1,414.40', record['Price'])
+        self.assertEqual('23:19:26', record['Time'])
+        self.assertEqual('20.00', record['Commission'])
+
+
+
+    def verifyTradeRecord1(self, record):
+        """
+        first trade
+        """
+        self.assertEqual(len(record), 10)   # there should be 10 fields
+        self.assertEqual('NGN13 Comdty', record['BloombergTicker'])
+        self.assertEqual('Cover', record['Side'])
+        self.assertEqual(1, record['Quantity'])
+        self.assertAlmostEqual(3.84, record['Price'])
+        self.assertEqual(datetime(2013,6,7), record['TradeDate'])
+        self.assertEqual(datetime(2013,6,7), record['SettlementDate'])
+        self.assertEqual(datetime(2013,6,7,hour=19,minute=1,second=1), record['Datetime'])
+
+
+
+    def verifyTradeRecord2(self, record):
+        """
+        first trade
+        """
+        self.assertEqual(len(record), 10)   # there should be 10 fields
+        self.assertEqual('S K4 Comdty', record['BloombergTicker'])
+        self.assertEqual('Short', record['Side'])
+        self.assertEqual(2, record['Quantity'])
+        self.assertAlmostEqual(1262, record['Price'])
+        self.assertEqual(datetime(2014,1,31), record['TradeDate'])
+        self.assertEqual(datetime(2014,1,31), record['SettlementDate'])
+        self.assertEqual(datetime(2014,1,31,hour=22,minute=47,second=36), record['Datetime'])
